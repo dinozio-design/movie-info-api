622ff7f4e263de3169f914fb38d154e45de511cf
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-11-29 23:37:39 -0500

    building the props

--- src/components/GuiContainer.jsx
+++ src/components/GuiContainer.jsx
@@ -3,19 +3,24 @@ import Row from "./Row";
 import Column from "./Column";
 import Container from "./Container";
 import Card from "./Card";
+import SearchForm from "./SearchForm";
 
 export default function GuiContainer() {
 
     // will put logic here with state and hooks
 
-    
+
 
     return (
         <Container>
             <Row>
                 <Column size="md-4">
-                    <Card>
-                        <p>this is a placeholder for form input</p>
+                    <Card heading="Search">
+                        <SearchForm
+                            // value={search} {/* next is to implement the useState and set the value of search */}
+                            // handleInputChange={handleInputChange} {/* need to write the function for this */}
+                            // handleFormSubmit={handleFormSubmit} {/* need to write the function for this */}
+                        />
                     </Card>
                 </Column>
                 <Column size="md-8">
