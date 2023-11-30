ca137f3eeb00f04797cc40a17101b717cf06b847
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-11-29 22:05:22 -0500

    rows and columns defined. now to define container and test them all in the layout

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function Row(props) {
+export default function Row(props) {
   return (
     <div className={`row${props.fluid ? '-fluid' : ''}`}>{props.children}</div>
   );
-}
-
-export default Row;
+};
