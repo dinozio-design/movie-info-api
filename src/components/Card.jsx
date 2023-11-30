a8f8409dfcb137f623606c2e413852eefc45ecae
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-11-30 13:01:09 -0500

    rewrote Card, MovieInfo, and GuiContainer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-export default function Card(){
-    return(
+export default function Card(props) {
+    return (
         <div className="card text-centre">
             <div className="card-header">
-                <h4>card heading goes here</h4>
+                <h2>{props.heading}</h2>
             </div>
-            <div className="card-body">
-                cardbody goes here
-            </div>
+            <div className="card-body">{props.children}</div>
         </div>
     );
 };
--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-export default function MovieInfo(props){
-    return(
+export default function MovieInfo(props) {
+    return (
         <div>
-            <img 
-            src={props.src} 
-            alt={props.title} 
+            <img
+                alt={props.title}
+                className="img-fluid"
+                src={props.src}
+                style={{ margin: '0 auto' }}
             />
+            <h3>Director(s): {props.director}</h3>
+            <h3>Genre: {props.genre}</h3>
+            <h3>Released: {props.released}</h3>
         </div>
     )
 };
